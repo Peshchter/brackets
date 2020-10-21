268fa56afa5ba60ff3494ffebe462e0960ca999c
Peshchter <leniviy.putnik@gmail.com>
2020-10-21 21:01:48 +0400

    feat: add export function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let a = []
+    let opens = [],
+        closes = []
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        opens.push(bracketsConfig[i][0])
+        closes.push(bracketsConfig[i][1])
+    }
+    let char, last, idx
+    debugger
+    for (let i = 0; i < str.length; i++) {
+        char = str.slice(i, i + 1)
+        if (closes.includes(char) && last == closes.indexOf(char)) {
+            a.pop()
+            last = a[ a.length - 1 ]
+        } else {
+            idx = opens.indexOf(char)
+            a.push(idx)
+            last = idx
+        }
+    }
+    return a.length ? false : true
 }
